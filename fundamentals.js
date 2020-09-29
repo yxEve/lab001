842c8201286ffb5be0d3493c6b67d9dfb5df6c29
Yuxin Nie <1371454048@qq.com>
2020-09-29 17:09:00 +0100

    Nested objects

--- fundamentals.js
+++ fundamentals.js
@@ -1,7 +1,17 @@
 const aCar = {
     owner : "Joe Bloggs",
-    type : 'Toyota Corolla 1.8',
-    registration : '201WD1058'
+    type : {
+        make : 'Toyota',
+        model : 'Corolla',
+        cc : 1.8
+    },
+    registration : {
+        year : 201,
+        county_code : 'WD',
+        number : 1058
+    }
 };
 
-console.log(aCar.owner);
+console.log(aCar.owner + ' drives a ' + aCar.type.make);
+console.log('Reg. = ' + aCar.registration.year + '-' + aCar.registration.county_code
++ '-' + aCar.registration.number);
